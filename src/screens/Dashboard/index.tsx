7744a179acb6d8477bd2442fc6152a4faffb6534
Bernardo V. Resende <bv.resende@gmail.com>
2022-01-10 19:11:02 -0300

    first commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from '../Dashboard/styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  )
+}
